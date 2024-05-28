1e9ae10885f4409d06a5c678c96ee15c6711adab
ribeyra <ribeyra@gmail.com>
2024-05-28 14:44:00 +0300

    lesson 5

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from hexlet_django_blog.article import views
 
+app_name = 'article'  # Задаем пространство имен для маршрутов приложения
+
 urlpatterns = [
     path('', views.index),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render     # noqa f401
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse     # noqa f401
 
 
 def index(request):
-    return HttpResponse('article')
+    app_name = request.resolver_match.app_name
+    # return HttpResponse('article')
+    return render(
+        request,
+        'articles/index.html',
+        context={
+            'app_name': app_name
+        }
+    )
