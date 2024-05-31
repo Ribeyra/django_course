619245248f9bb2a3571471edcb31fbcbe7d6c914
ribeyra <ribeyra@gmail.com>
2024-05-31 15:14:00 +0300

    lesson 12

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+from .models import Article
 
 # Register your models here.
+
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # Перечисляем поля, отображаемые в таблице списка статей
+    list_display = ('name', 'timestamp')
+
+    search_fields = ['name', 'body']
+
+    # Перечисляем поля для фильтрации
+    list_filter = (('timestamp', DateFieldListFilter),)
+
+
+# admin.site.register(Article, ArticleAdmin)
